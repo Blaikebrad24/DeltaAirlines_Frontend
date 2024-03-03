69757243e64c11e6cf0feaf37680a5d2c55e9cba
Blaike Bradford <blaikebradford@Blaikes-Mini.attlocal.net>
2024-03-03 11:42:43 -0600

    FC component for DeltaLogo implemented

--- delta_airlines_frontend/components/Changers/headerLogoImg.tsx
+++ delta_airlines_frontend/components/Changers/headerLogoImg.tsx
@@ -1,25 +1,41 @@
-import React from 'react';
+'use client'
+
+import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons'; // Import the FontAwesome icon you want to use
 
-interface Props {
-  width: number;
-  height: number;
-}
 
-const MyComponent: React.FC<Props> = ({ width, height }) => {
-  const isSmallScreen = width < 768;
 
+const HeaderLogoImg: React.FC = () => {
+  const [isSmallScreen, setSmallScreen] = useState(false);
+
+  //useEffect Hook
+
+  useEffect(() =>{
+
+    const handleResize = () =>{
+    const screenWidth = window.innerWidth;
+    console.log(screenWidth);
+    
+    if(screenWidth < 768){setSmallScreen(true)}
+    else{setSmallScreen(false)} 
+    }
+    window.addEventListener('resize', handleResize);
+    handleResize();
+    return () => window.removeEventListener('resize', handleResize);
+
+    },
+    []);
   return (
     <div>
       {isSmallScreen ? (
         <FontAwesomeIcon icon={faBars} />
       ) : (
-        <Image src="//images/delaLogo.png" alt="Large Image" width={500} height={300} />
+        <Image src="/images/delaLogo.png" alt="Large Image" width={500} height={300} />
       )}
     </div>
   );
 };
 
-export default MyComponent;
+export default HeaderLogoImg;
--- delta_airlines_frontend/components/Header/Header.tsx
+++ delta_airlines_frontend/components/Header/Header.tsx
@@ -1,17 +1,12 @@
 import Image from 'next/image'
 import React from 'react'
+import HeaderLogoImg from '../Changers/headerLogoImg'
 
 function Header() {
   return (
     <header className='fixed w-full z-20 top-0 flex items-center justify-between p-5 bg-deltaBlue min-w-[570px]'>
         <div className='ml-5 h-auto  w-1/5 resize-none'>
-            <Image
-             className=' flex  '
-             src="/images/delaLogo.png"
-             height={50}
-             width={150}
-             alt='Delta Clone'
-             />
+          <HeaderLogoImg/>
         </div>
 
         <div className=' hidden md:inline-block w-1/2 items-center'>Delta Toolbar Options</div>
