855337c5a716eccfb901c8a64ea5bdd8422a744e
Blaike Bradford <blaikebradford@Blaikes-Mini.attlocal.net>
2024-03-16 18:40:30 -0500

    New push update

--- delta_airlines_frontend/components/Sections/HeroSection/HeroCardSection.tsx
+++ delta_airlines_frontend/components/Sections/HeroSection/HeroCardSection.tsx
@@ -12,7 +12,7 @@ const { selectedTab } = useHeroContext();
         <div className=" text-toothWhite absolute bottom-0 w-3/4 h-2/3 bg-testGreen">
                     
         {/* <BookFullPageCard/> */}
-        {selectedTab === 'BOOK' && <div>Booking Form</div>}
+        {selectedTab === 'BOOK' && <div>Booking Form Here</div>}
         {selectedTab === 'CHECK-IN' && <div>Check-In Form</div>}
         {selectedTab === 'MY TRIPS' && <div>My Trips Info</div>}
         {selectedTab === 'FLIGHT STATUS' && <div>Flight Status</div>}
